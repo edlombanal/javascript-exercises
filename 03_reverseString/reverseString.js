9b96acd190f679d60cf06e79ef37d0073ecf3f58
Eduardo Lombana <edlombanal@gmail.com>
2022-04-24 18:18:56 -0500

    Solve reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let strArr = str.split('');
+    let strLen = strArr.length;
+    let newStr = '';
+    for(let i = strLen - 1; i>=0; i--){
+        newStr += strArr[i];
+    };
+    return newStr;
 };
 
 // Do not edit below this line
