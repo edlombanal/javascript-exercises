7b07cc83c2f286bc8b745264a2102b42ecf4081f
Eduardo Lombana <edlombanal@gmail.com>
2022-04-24 17:27:56 -0500

    Solve repeat string exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let repeat = '';
+    if(num>=0){
+        for(let i=1; i<=num; i++){
+            repeat += str;
+        };
+        return repeat;
+    }else{
+        return 'ERROR';
+    };
 };
 
 // Do not edit below this line
