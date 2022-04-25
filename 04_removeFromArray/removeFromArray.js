84f03ada04e4e1677bc61ec3876141d801be1ae3
Eduardo Lombana <edlombanal@gmail.com>
2022-04-24 20:56:23 -0500

    Solve remove from array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(Arr,...removeFromArr) {
+    let mainArr = Arr;
+    let index;
+    for(let i=0; i<removeFromArr.length; i++){
+        index = mainArr.findIndex(pos => pos===removeFromArr[i]);
+        if(index!==-1){
+            mainArr.splice(index,1);
+        };
+    };
+    return mainArr;
 };
 
 // Do not edit below this line
