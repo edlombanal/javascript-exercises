c518c9900280b3d179d9a9e6c1b394dc1ec2e1d5
Eduardo Lombana <edlombanal@gmail.com>
2022-04-27 18:29:28 -0500

    Solve temp conversion exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let celcius = (temp-32)*(5/9);
+  return parseFloat(celcius.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let faren = temp*(9/5)+32;
+  return parseFloat(faren.toFixed(1));
 };
 
 // Do not edit below this line
