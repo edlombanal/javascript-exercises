28496226d73372c8061b9063f1202da3857124b4
Eduardo Lombana <edlombanal@gmail.com>
2022-04-26 20:33:41 -0500

    Solve leap years exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,19 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    //define divisions
+    let div100 = year%100===0;
+    let div400 = year%400===0;
+    let div4 = year%4===0;
+
+    //conditionals
+    if(div400){
+        return true;
+    }else if(div100){
+        return false;
+    }else if(div4){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
